c553e0330c4f2666c0deaf80a3005767a17a122c
AY-BAMZ <aybamz181@gmail.com>
2020-09-03 15:22:07 +0100

    Updates

---
+++ precache-manifest.1f0edd0e173c58656403f5cd3d50c5d5.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf49864cacaec66913900736a0c5665",
+    "url": "/AY-BAMZ/AboutBamzz.git/index.html"
+  },
+  {
+    "revision": "b43f23d5f58ff17cd653",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/css/2.6be9f041.chunk.css"
+  },
+  {
+    "revision": "9f2daf7823a05190c68a",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/css/main.d751c487.chunk.css"
+  },
+  {
+    "revision": "b43f23d5f58ff17cd653",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/js/2.ea04ae54.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/js/2.ea04ae54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2daf7823a05190c68a",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/js/main.60d6a990.chunk.js"
+  },
+  {
+    "revision": "0621bdfd81d7d6677b8d",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/js/runtime-main.e58f0741.js"
+  },
+  {
+    "revision": "dd6a8d2b6b6fb6db44c30f1a9bd13cc1",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/1.dd6a8d2b.jpg"
+  },
+  {
+    "revision": "7f7b685a8243a443db089996cf3b9497",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/13.7f7b685a.png"
+  },
+  {
+    "revision": "75da7fa288c485e117454f9b2eb1a420",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/2.75da7fa2.jfif"
+  },
+  {
+    "revision": "b810d479326042b04464391a7df9e575",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/3.b810d479.jpg"
+  },
+  {
+    "revision": "8f0cac50a6f8a1d8c29aeea98a3272db",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/4.8f0cac50.jfif"
+  },
+  {
+    "revision": "9ed6b5d9db96c801a896ec95b1d08727",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/5.9ed6b5d9.jpg"
+  },
+  {
+    "revision": "90a57945b41bb81c89583e78e55f574e",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/6.90a57945.jfif"
+  },
+  {
+    "revision": "11afa7ef309509054ff1776c3cf34bd7",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/7.11afa7ef.jpg"
+  },
+  {
+    "revision": "d4c44d603a9dd4845fea709886c8dc4b",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/8.d4c44d60.png"
+  },
+  {
+    "revision": "059f6232525243e9950f95aaf0d3e97c",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/design1.059f6232.svg"
+  },
+  {
+    "revision": "ac784946c687b9f03bf90e13d76a0715",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/design2.ac784946.svg"
+  },
+  {
+    "revision": "dc4527f5e4c474854ff0ab34123205e4",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/design3.dc4527f5.svg"
+  },
+  {
+    "revision": "1c79dbaa2502eb5ed93dad18fd240611",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/design4.1c79dbaa.svg"
+  },
+  {
+    "revision": "17a094a373575f073504b07a07ec590c",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/facebook.17a094a3.svg"
+  },
+  {
+    "revision": "2371f4293673e26527d53c626b832912",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/instagram.2371f429.svg"
+  },
+  {
+    "revision": "f7b69b2ccb889c4f1e0a269fd175832c",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/quality.f7b69b2c.svg"
+  },
+  {
+    "revision": "5b3b0edd4b8bcc1e94e031073ddba34a",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/quality1.5b3b0edd.svg"
+  },
+  {
+    "revision": "9aa2097313618d8c84fa6cc8096b5cfd",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/quality2.9aa20973.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "4c47721daf8c312effe677be39b720da",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/twitter.4c47721d.svg"
+  },
+  {
+    "revision": "36d3147deabb995fb114b88f9bf220b7",
+    "url": "/AY-BAMZ/AboutBamzz.git/static/media/whatsapp.36d3147d.svg"
+  }
+]);
